361624d93417e95b1c331629df92f2fa52f4a882
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 01:23:58 +0300

    add testing page

---
+++ src/pages/testing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const testing = () => {
+  return (
+    <Layout>
+      <Seo title="Standardization" />
+    </Layout>
+  );
+};
+
+export default testing;
