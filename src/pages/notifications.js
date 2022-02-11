26041560eb0bce18a12a05a92a9eac2486f7c9dc
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 01:29:56 +0300

    add notifications page

---
+++ src/pages/notifications.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const notifications = () => {
+  return (
+    <Layout>
+      <Seo title="Notifications" />
+    </Layout>
+  );
+};
+
+export default notifications;
