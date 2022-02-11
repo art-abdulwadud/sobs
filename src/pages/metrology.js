a5fd0b2eccb3fa101c2bb52135e98ad8f5d01b3a
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 01:27:41 +0300

    add metrology page

---
+++ src/pages/metrology.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const metrology = () => {
+  return (
+    <Layout>
+      <Seo title="Certification" />
+    </Layout>
+  );
+};
+
+export default metrology;
