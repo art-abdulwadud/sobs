495fb49d5bbab49c94df99db681fecb3cdaa3e62
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 01:22:58 +0300

    add standardization page

---
+++ src/pages/standardization.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const standardization = () => {
+  return (
+    <Layout>
+      <Seo title="Standardization" />
+    </Layout>
+  );
+};
+
+export default standardization;
