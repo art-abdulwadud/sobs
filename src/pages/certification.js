e5a53c762616dee296d055970b92537ec2119714
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 01:26:39 +0300

    add certification page

---
+++ src/pages/certification.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const certification = () => {
+  return (
+    <Layout>
+      <Seo title="Certification" />
+    </Layout>
+  );
+};
+
+export default certification;
