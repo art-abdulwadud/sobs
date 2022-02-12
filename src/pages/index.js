6958cc6438e0ace3b0c9feb7d8043071111957e7
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 05:52:30 +0300

    display services component in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import Layout from '../components/layout';
 import NavBar from '../components/navbar/NavBar';
 import Seo from '../components/seo';
+import Services from '../components/services/Services';
 
 const IndexPage = () => {
   return (
     <Layout>
       <Seo title="Home" />
       <NavBar />
+      <Services />
     </Layout>
   );
 };
