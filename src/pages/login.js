f4fb803e4ad8f5aaa6163327615d932914cd0a57
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 04:28:44 +0300

    add login component

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Toast } from 'primereact/toast';
+import { connect } from 'react-redux';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
-const login = () => {
+const login = ({ toast }) => {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Seo title="Login" />
+      <Toast ref={toast} position="top-right" className="text-left m-w-300" />
+    </Layout>
   );
 };
 
-export default login;
+const mapStateToProps = (state) => ({
+  toast: state.auth.toast
+});
+
+export default connect(mapStateToProps)(login);
