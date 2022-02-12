6c59e275626881e6cf0ccb55896a92ae3950ce67
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-12 04:30:51 +0300

    display login and login component

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,12 +3,14 @@ import { Toast } from 'primereact/toast';
 import { connect } from 'react-redux';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import Login from '../components/login/Login';
 
 const login = ({ toast }) => {
   return (
     <Layout>
       <Seo title="Login" />
       <Toast ref={toast} position="top-right" className="text-left m-w-300" />
+      <Login />
     </Layout>
   );
 };
