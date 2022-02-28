6f42a05f79b77af745604f6f70b719304ccaceda
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-28 14:56:45 +0300

    change lorem text with text content in services component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -5,52 +5,51 @@ const Services = () => {
   return (
     <div className="surface-section px-4 py-4 md:px-6 lg:px-8 text-center linear-bg">
       <div className="mb-3 font-bold text-2xl">
-        <span className="text-900 dark-c">One Product, </span>
-        <span className="text-900 text-white">Many Solutions</span>
+        <span className="text-900 text-white">Conformity Assessment Portal</span>
       </div>
-      <div className="honeydew text-sm mb-6 f3">Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.</div>
+      <div className="honeydew text-sm mb-6 f3">Promoting Conformity Assessment Schemes in Somalia</div>
       <div className="grid">
         <div className="col-12 md:col-4 mb-4 px-5 cursor" onClick={() => navigate('/standardization')}>
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-book text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">STANDARDIZATION</div>
-          <span className="honeydew text-sm line-height-3">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
+          <span className="honeydew text-sm line-height-3">Purchase Somali and International Standards Now.</span>
         </div>
         <div className="col-12 md:col-4 mb-4 px-5 cursor">
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-check-circle text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">INSPECTION</div>
-          <span className="honeydew text-sm line-height-3">Risus nec feugiat in fermentum posuere urna nec. Posuere sollicitudin aliquam ultrices sagittis.</span>
+          <span className="honeydew text-sm line-height-3">Apply for Consignment Based Conformity Assessment inspection now.</span>
         </div>
         <div className="col-12 md:col-4 mb-4 px-5 cursor">
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-filter text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">TESTING</div>
-          <span className="honeydew text-sm line-height-3">Ornare suspendisse sed nisi lacus sed viverra tellus. Neque volutpat ac tincidunt vitae semper.</span>
+          <span className="honeydew text-sm line-height-3">Apply for Sample or Batch Testing now.</span>
         </div>
         <div className="col-12 md:col-4 mb-4 px-5 cursor">
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-id-card text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">CERTIFICATION</div>
-          <span className="honeydew text-sm line-height-3">Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus.</span>
+          <span className="honeydew text-sm line-height-3">Apply for product certification to expedite credible trade.</span>
         </div>
         <div className="col-12 md:col-4 mb-4 px-5 cursor">
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-moon text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">METROLOGY</div>
-          <span className="honeydew text-sm line-height-3">Nec tincidunt praesent semper feugiat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. </span>
+          <span className="honeydew text-sm line-height-3">Apply for calibration of measurements and metrology services.</span>
         </div>
         <div className="col-12 md:col-4 md:mb-4 mb-0 px-3 cursor">
           <span className="p-3 shadow-2 mb-3 inline-block surface-card" style={{ borderRadius: '10px' }}>
             <i className="pi pi-star text-4xl text-blue-500"></i>
           </span>
           <div className="text-900 mb-3 font-medium">ACCREDITATION</div>
-          <span className="honeydew text-sm line-height-3">Mattis rhoncus urna neque viverra justo nec ultrices. Id cursus metus aliquam eleifend.</span>
+          <span className="honeydew text-sm line-height-3">Apply for accreditation and gain recognition now.</span>
         </div>
       </div>
     </div>
