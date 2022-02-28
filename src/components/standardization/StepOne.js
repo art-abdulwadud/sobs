7a00946f2dec675f1439ae79daaa4bfb38d46c7c
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-02-28 16:44:30 +0300

    replace stepone with primeblock

--- src/components/standardization/StepOne.js
+++ src/components/standardization/StepOne.js
@@ -1,79 +1,70 @@
 import React from 'react';
 import { Button } from 'primereact/button';
-import image from '../../images/bg_images/standards.jpeg';
 
 const isBrowser = typeof window !== 'undefined';
 
 const StepOne = () => {
   return (
     <li className="pt-4 pe-2 ps-2 w-100 h-100" style={{ overflow: 'hidden' }}>
-      <div className="d-flex center w-100 column" style={{ minHeight: '80%' }}>
-        <div
-          className="grid grid-nogutter surface-section text-800 shadow-5 w-100"
-          style={{ minHeight: '80vh' }}
-        >
-          <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
-            <section>
-              <span className="block text-6xl font-bold mb-1">Explore</span>
-              <div className="text-6xl text-primary font-bold mb-3 f2">
-                Standards
-              </div>
-              <p className="mt-0 mb-4 text-700 line-height-3">
-                In Somalia, SOBS (Somali Bureau of Standards) is the sole body
-                responsible for the production, development, adoption and
-                publication of standards that provide requirements for
-                materials, manufacturing, testing, packaging and labelling to
-                ensure that products, processes and services are fit for their
-                purpose standardization involves the development and provision
-                of standards, and the supply of information on standards to
-                interested parties, and takes place on various levels.
-              </p>
-            </section>
+      <div className="surface-section px-4 py-8 md:px-6 lg:px-8">
+        <div className="grid">
+          <div className="col-12 md:col-6">
+            <span className="block text-6xl font-bold mb-1">Explore</span>
+            <div className="text-6xl text-primary font-bold mb-3 f2">
+              Standards
+            </div>
+            <p className="mt-0 mb-4 text-700 line-height-3">
+              In Somalia, Somali Bureau Of Standards Is The Sole Body
+              Responsible For The Production, Development, Adoption And
+              Publication Of Standards That Provide Requirements For Materials,
+              Manufacturing, Testing, Packaging And Labeling To Ensure That
+              Products, Processes And Services Are Fit For Their Purpose.
+            </p>
           </div>
-          <div
-            className="col-12 md:col-6 overflow-hidden d-flex center column pb-3"
-            style={{
-              alignItems: 'center',
-              justifyContent: 'end',
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${image})`,
-              backgroundSize: 'cover'
-            }}
-          >
-            <section className="d-flex column">
-              <Button
-                label="Learn more about Standards"
-                className="p-button-rounded white-bg-h dark-c mb-3 f3"
-                style={{ fontSize: '1.3em', minWidth: '400px' }}
-                onClick={(ev) => {
-                  ev.preventDefault();
-                  isBrowser && typeof document !== 'undefined'
-                    ? document.querySelector('#step-two').scrollIntoView()
-                    : null;
-                }}
-              />
-              <Button
-                label="Somali Standards Catalogue"
-                className="p-button-rounded white-bg-h dark-c mb-3 f3"
-                onClick={(ev) => {
-                  ev.preventDefault();
-                  isBrowser && typeof document !== 'undefined'
-                    ? document.querySelector('#step-three').scrollIntoView()
-                    : null;
-                }}
-                style={{ fontSize: '1.3em', minWidth: '400px' }}
-              />
-              <Button
-                label=" International Standards Catalogue"
-                className="p-button-rounded white-bg-h dark-c f3"
-                style={{ fontSize: '1.3em', minWidth: '400px' }}
-                onClick={(ev) => {
-                  ev.preventDefault();
-                  isBrowser && typeof document !== 'undefined'
-                    ? document.querySelector('#step-four').scrollIntoView()
-                    : null;
-                }}
-              />
-            </section>
+          <div className="col-12 md:col-6 flex align-items-center md:justify-content-end">
+            <div>
+              <section className="d-flex column">
+                <Button
+                  label="Learn more about Standards"
+                  iconPos="right"
+                  icon="pi pi-chevron-right"
+                  className="p-button-rounded linear-bg-h mb-3 f3 shadow-4"
+                  style={{ fontSize: '1.3em', minWidth: '400px' }}
+                  onClick={(ev) => {
+                    ev.preventDefault();
+                    isBrowser && typeof document !== 'undefined'
+                      ? document.querySelector('#step-two').scrollIntoView()
+                      : null;
+                  }}
+                />
+                <Button
+                  label="Somali Standards Catalogue"
+                  iconPos="right"
+                  icon="pi pi-chevron-right"
+                  className="p-button-rounded linear-bg-h mb-3 f3 shadow-4"
+                  onClick={(ev) => {
+                    ev.preventDefault();
+                    isBrowser && typeof document !== 'undefined'
+                      ? document.querySelector('#step-three').scrollIntoView()
+                      : null;
+                  }}
+                  style={{ fontSize: '1.3em', minWidth: '400px' }}
+                />
+                <Button
+                  label=" International Standards Catalogue"
+                  iconPos="right"
+                  icon="pi pi-chevron-right"
+                  className="p-button-rounded linear-bg-h f3 shadow-4"
+                  style={{ fontSize: '1.3em', minWidth: '400px' }}
+                  onClick={(ev) => {
+                    ev.preventDefault();
+                    isBrowser && typeof document !== 'undefined'
+                      ? document.querySelector('#step-four').scrollIntoView()
+                      : null;
+                  }}
+                />
+              </section>
+            </div>
           </div>
         </div>
       </div>
