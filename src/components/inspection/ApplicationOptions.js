2d62857334f5a8aa40eebcbd57d40606dbe7931e
ahmed14-cell <ahmedito14@gmail.com>
2022-05-28 15:40:13 +0300

    refactor: replace options

--- src/components/inspection/ApplicationOptions.js
+++ src/components/inspection/ApplicationOptions.js
@@ -6,8 +6,8 @@ const ApplicationOptions = () => {
     <>
       <div className="grid mt-5 p-2">
         <SingleOption option="Application History" optionCount="0 Applications" />
-        <SingleOption option="Application History" optionCount="0 Applications" />
-        <SingleOption option="Application History" optionCount="0 Applications" />
+        <SingleOption option="New Application" optionCount="0 Applications" />
+        <SingleOption option="Quick Links" optionCount="0 Applications" />
         <SingleOption option="Application History" optionCount="0 Applications" />
       </div>
     </>
